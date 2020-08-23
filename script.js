6db0c5ac6551d0987c92325d5fb01465ad394b0f
abiryusuf <yusuf_ctg@yahoo.com>
2020-08-23 13:09:08 -0500

    html and css

---
+++ script.js
@@ -0,0 +1,8 @@
+const seven = document.getElementById("seven");
+
+seven.addEventListener("click", function(){
+    var valueInput = document.getElementById("input");
+    var numSeven = parseInt(valueInput.value);
+
+    console.log(numSeven)
+})
